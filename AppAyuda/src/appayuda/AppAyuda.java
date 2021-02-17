157cab59af8ca401cfac45fd2fa85a83d95720db
ShadowMcPollo7 <raul-moreno1703@hotmail.com>
2021-02-17 14:16:15 +0100

    AppAyuda y Help

---
+++ AppAyuda/src/appayuda/AppAyuda.java
@@ -0,0 +1,30 @@
+package appayuda;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author raul-
+ */
+public class AppAyuda extends Application {
+
+    private Scene scene;
+
+    @Override
+    public void start(Stage stage) {
+        // create the scene
+        stage.setTitle("Web View");
+        scene = new Scene(new Browser(), 750, 500, Color.web("#666970"));
+        stage.setScene(scene);
+        //scene.getStylesheets().add(AppAyuda.class.getResource("BrowserToolbar.css").toExternalForm());
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
